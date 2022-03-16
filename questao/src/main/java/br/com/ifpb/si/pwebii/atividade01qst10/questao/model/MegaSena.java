a4ac5fc11b6a6cb506cb1cc0e8e932513199f784
lAlissial <alissialima@hotmail.com>
2022-03-16 05:38:54 -0300

    adicionando

--- questao/src/main/java/br/com/ifpb/si/pwebii/atividade01qst10/questao/model/MegaSena.java
+++ questao/src/main/java/br/com/ifpb/si/pwebii/atividade01qst10/questao/model/MegaSena.java
@@ -1,5 +1,34 @@
 package br.com.ifpb.si.pwebii.atividade01qst10.questao.model;
 
+import java.util.List;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.Random;
+
+import lombok.AllArgsConstructor;
+
+
+@AllArgsConstructor
 public class MegaSena {
+    private int quantidade;
+    private List<Integer> numeros_aleatorios = new ArrayList<Integer>();
+
     
+    public void geraNumerosAleatorios(){
+        int validos = this.numeros_aleatorios.size();
+        Random random = new Random();
+
+        
+        for(int j=0; j < quantidade; j++){
+            int aleatorio = random.nextInt(61);
+            for (int i=0; i < quantidade; i++){
+                if (aleatorio == this.numeros_aleatorios.get(i) && i != j){
+                    aleatorio = random.nextInt(61);
+                } else {
+                    this.numeros_aleatorios.add(aleatorio)
+                }
+            }
+        }
+        Collections.sort(this.numeros_aleatorios);
+    }
 }
