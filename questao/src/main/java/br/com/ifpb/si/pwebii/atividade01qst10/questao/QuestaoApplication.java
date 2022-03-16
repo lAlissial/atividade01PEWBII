be1e80a54f7f91c6a10ea9c6791392b309d1a2bf
lAlissial <alissialima@hotmail.com>
2022-03-15 23:02:34 -0300

    Initial commit

---
+++ questao/src/main/java/br/com/ifpb/si/pwebii/atividade01qst10/questao/QuestaoApplication.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.si.pwebii.atividade01qst10.questao;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class QuestaoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(QuestaoApplication.class, args);
+	}
+
+}
---
+++ questao/src/test/java/br/com/ifpb/si/pwebii/atividade01qst10/questao/QuestaoApplicationTests.java
@@ -0,0 +1,13 @@
+package br.com.ifpb.si.pwebii.atividade01qst10.questao;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class QuestaoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
