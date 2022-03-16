342be51e49a54bbb659144a48793565a57f30dfd
lAlissial <alissialima@hotmail.com>
2022-03-15 23:58:47 -0300

    add

---
+++ questao/src/main/java/br/com/ifpb/si/pwebii/atividade01qst10/questao/controller/MegaSenaController.java
@@ -0,0 +1,15 @@
+package br.com.ifpb.si.pwebii.atividade01qst10.questao.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import br.com.ifpb.si.pwebii.atividade01qst10.questao.model.MegaSena;
+
+@Controller
+@RequestMapping("/megasena")
+public class MegaSenaController {
+	
+	private MegaSena mega_sena;
+
+}
---
+++ questao/src/main/java/br/com/ifpb/si/pwebii/atividade01qst10/questao/model/MegaSena.java
@@ -0,0 +1,5 @@
+package br.com.ifpb.si.pwebii.atividade01qst10.questao.model;
+
+public class MegaSena {
+    
+}
